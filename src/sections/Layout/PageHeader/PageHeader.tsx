7377cdc718e5646c9db6ac1ecc683cf76dcfdb3d
nachoDev <ignacio.farias.v@usach.cl>
2024-01-03 17:33:37 -0300

    feat(header): add actions icon based buttons to the page header component

--- src/sections/Layout/PageHeader/PageHeader.tsx
+++ src/sections/Layout/PageHeader/PageHeader.tsx
@@ -1,20 +1,30 @@
-import { Menu } from 'lucide-react';
+import { Bell, Menu, Upload, User } from 'lucide-react';
 import logo from '@/assets/pokemon-logo.png';
 import { Button } from '@/components';
 
 export default function PageHeader() {
   return (
-    <div className="flex justify-between gap-10 lg:gap-20">
+    <div className="mx-4 mb-6 flex justify-between gap-10 pt-2 lg:gap-20">
       <div className="flex shrink-0 items-center gap-4">
         <Button variant="ghost" size="icon">
           <Menu />
         </Button>
         <a href="/">
-          <img src={logo} alt="Pokemon logo" className="h-6" />
+          <img src={logo} alt="Pokemon logo" className="h-12" />
         </a>
       </div>
       {/* <div></div> */}
-      {/* <div></div> */}
+      <div className="flex shrink-0 md:gap-2 ">
+        <Button variant="ghost" size="icon">
+          <Upload />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Bell />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <User />
+        </Button>
+      </div>
     </div>
   );
 }
